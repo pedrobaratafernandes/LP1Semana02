afe57ae3665f7a4750ec0572b78abb14be6fcefd
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-20 06:24:25 +0000

    pedir altura

---
+++ Cilindro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Altura: ");
+            double altura = double.Parse(Console.ReadLine());
+        }
+    }
+}
