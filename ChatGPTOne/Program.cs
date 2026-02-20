e09268ea3978571cb58b0e344142441fe4056fc1
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-20 14:30:30 +0000

    ver e fazer exercicios

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string questao;
+            do
+            {
+                Console.Write("Coloque uma questão: ");
+                questao = Console.ReadLine();
+                string resposta;
+
+                DateTime dateTime = DateTime.Now;
+                switch (questao)
+                {
+                    case "esta a chover ?":
+                        resposta = "não";
+                        break;
+                    case "que dia é hoje ?":
+                        resposta = DateTime.Now.ToString("dd");
+                        break;
+                    case "que ano é ?":
+                        resposta = DateTime.Now.ToString("yyyy");
+                        break;
+                    case "que horas sao ?":
+                        resposta = dateTime.ToString("HH:mm:ss");
+                        break;
+                    case "EXIT":
+
+                        continue;
+                    default:
+                        resposta = "nao tenho resposta para essa questão";
+                        break;
+                }
+                Console.WriteLine(resposta);
+            } while (questao != "EXIT");
+        }
+    }
+}
