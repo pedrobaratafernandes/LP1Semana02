5415e4a9098ee5032618955e75a0d7078fbf9be9
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-20 06:19:37 +0000

    Cria um programa que pede ao utilizador um número inteiro não negativo, faz a conversão necessária e guarda o valor numa variável n do tipo byte, e mostra no ecrã o seguinte: → Divisão por 2 → Shift left por 3 → XOR com 6 → Se é maior que 10

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro-não negativo: ");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                Console.WriteLine(n / 2);
+                Console.WriteLine(n << 3);
+                Console.WriteLine(n ^ 6);
+                Console.WriteLine(n > 10 ? true : false);
+            }
+            else
+            {
+                Console.WriteLine("numero negativo");
+            }
+
+
+        }
+    }
+}
