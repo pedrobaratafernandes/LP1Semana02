2332690782204424bded31f88637784c70206624
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-20 06:29:32 +0000

    Apresenta o volume V desse cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,6 +10,8 @@ namespace Cilindro
             double a = double.Parse(Console.ReadLine());
             Console.Write("Raio: ");
             double r = double.Parse(Console.ReadLine());
+            double v = Math.PI * Math.Pow(r, 2) * a;
+            Console.WriteLine($"Volume: {v}");
         }
     }
 }
