84407e2dd537e35f7707664f613361b6173b75dd
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-20 06:09:56 +0000

    Cria um programa que pede ao utilizador um número inteiro, faz a conversão necessária e guarda o valor numa variável n do tipo sbyte, e mostra no ecrã o seguinte: * Resultado da operação de decremento de n * Resultado da operação de incremento de n

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
