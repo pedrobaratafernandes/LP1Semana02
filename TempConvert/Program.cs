413ea31b780a6611e772a62a4a364cbd98cd76ac
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-26 16:27:19 +0000

    segundo commit

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Reflection;
 namespace TempConvert
 {
     public class Program
@@ -11,8 +12,27 @@ namespace TempConvert
             float temperatura = float.Parse(Console.ReadLine());
             Console.Write("Unit (C/F): ");
             string medida = Console.ReadLine();
-            Console.Write($"{temperatura} {medida}");
-           
+            float total_cel;
+            float total_fah;
+
+            switch (medida)
+            {
+                case "C":
+                    total_fah = temperatura * 1.8f + 32;
+                    total_cel = (temperatura - 32) / 1.8f;
+                    Console.WriteLine($"{temperatura:f2} C = {total_fah:f2} F");
+                    Console.Write($"Absolute value: {total_fah:f2}");
+                    break;
+
+                case "F":
+                    total_cel = (temperatura - 32) / 1.8f;
+                    total_fah = temperatura * 1.8f + 32;
+                    Console.WriteLine($"{temperatura:f2} F = {total_cel:f2} C");
+                    float total = Math.Abs(total_fah);
+                    Console.Write($"Absolute value: {total:f2}");
+                    break;
+            }
+
         }
     }
 }
