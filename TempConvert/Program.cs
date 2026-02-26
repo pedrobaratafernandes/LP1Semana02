c3660c0c1268bbcc6b55887595f137cafcf621d1
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-02-26 16:08:29 +0000

    primeiro commit

---
+++ TempConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.Write("Temperature: ");
+            float temperatura = float.Parse(Console.ReadLine());
+            Console.Write("Unit (C/F): ");
+            string medida = Console.ReadLine();
+            Console.Write($"{temperatura} {medida}");
+           
+        }
+    }
+}
